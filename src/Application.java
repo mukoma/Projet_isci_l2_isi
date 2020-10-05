49b05a3548869fb474c5189802a33e5aaa6c1ba6
mukoma <mukomalagrace80@gmail.com>
2020-10-05 09:29:30 +0200

    premiere version du logi contenant sys

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bonjour tout le monde");
+		System.out.println("Bonjour tout le monde");
+	}
+
+}
